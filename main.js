02f49f5d2e61a5d5ba7377c3dd943e3e1c2d2d39
nangongjiujiu <363749768@qq.com>
2017-01-05 12:53:59 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$("#bilibili").click(function(){
+	window.open('http://space.bilibili.com/70650/');
+});
+$("#weibo").click(function(){
+	window.open('http://weibo.com/nangongjiujiu');
+});
+$("#codepen").click(function(){
+	window.open('http://codepen.io/992939504/');
+});
+$("#github").click(function(){
+	window.open('https://github.com/992939504/992939504.github.io');
+});
+});
